ec976b6e1eb53bcf97b402a85cc6b1d7c3d1bc45
Yasmine <yqueen@student.bridgew.edu>
2022-09-29 12:03:32 -0400

    Drawing

---
+++ main.py
@@ -0,0 +1,20 @@
+import arcade
+
+arcade.open_window(1200, 750, "Lots of shapes")
+arcade.set_background_color(arcade.color.JAPANESE_CARMINE)
+arcade.start_render()
+arcade.draw_line_strip([(20,30), (200,30), (300,200), (200,300), (20,40)], arcade.color.FERN_GREEN,3)
+arcade.draw_lrtb_rectangle_filled(320, 420, 300, 50, arcade.color.EBONY)
+arcade.draw_lrtb_rectangle_outline(450, 550, 300, 50, arcade.color.ENGLISH_GREEN, 12)
+arcade.draw_xywh_rectangle_filled(380, 120, 300, 80, arcade.color.FELDSPAR)
+arcade.draw_xywh_rectangle_outline(10, 10, 700, 300, arcade.color.NAPIER_GREEN, 8)
+arcade.draw_circle_filled(150, 600, 75, arcade.color.CHAMPAGNE)
+arcade.draw_circle_outline(150, 600, 125, arcade.color.CERULEAN, 5)
+arcade.draw_triangle_filled(300, 600, 600, 600, 450, 350, arcade.color.BONE)
+arcade.draw_triangle_outline(600, 600, 490, 300, 700, 750, arcade.color.BLAST_OFF_BRONZE, 6)
+arcade.draw_arc_filled(400, 700, 100, 100, arcade.color.BANANA_MANIA, 30,350)
+arcade.draw_arc_outline(980, 700, 200, 100, arcade.color.COQUELICOT, -180, 0, 9)
+arcade.draw_parabola_filled(900, 300, 1000, 100, arcade.color.CHAMOISEE)
+arcade.finish_render()
+
+arcade.run()
